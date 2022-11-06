bca013e2236b6b4c28e3a510d0b39843ec74bf90
Alfonso M. Evangelista <aevangelista@ofimatic.com>
2022-11-06 14:55:20 -0400

    no versionar el output

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,12 @@
         <NuxtWelcome />
     </div>
 </template>
+<script>
+export default {
+    mounted() {
+        const logo = document.getElementsByClassName("nuxt-logo")
+        logo[0].href = "/precing"
+        logo[0].target = ""
+    }
+}
+</script>
