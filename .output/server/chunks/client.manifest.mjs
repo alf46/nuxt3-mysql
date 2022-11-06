e5615a046f23ea0397dccd3e4e516fea256bd456
Alfonso M. Evangelista <aevangelista@ofimatic.com>
2022-11-06 14:06:09 -0400

    Add output files

---
+++ .output/server/chunks/client.manifest.mjs
@@ -0,0 +1,111 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "entry.baa61d88.js",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "virtual:nuxt:C:/Users/a46ev/Desktop/nuxt3/n3mysql/.nuxt/error-component.mjs"
+    ]
+  },
+  "virtual:nuxt:C:/Users/a46ev/Desktop/nuxt3/n3mysql/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-component.459a892b.js",
+    "src": "virtual:nuxt:C:/Users/a46ev/Desktop/nuxt3/n3mysql/.nuxt/error-component.mjs",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ]
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.9d5882c0.js",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": [
+      "index.eba111bf.css"
+    ]
+  },
+  "index.eba111bf.css": {
+    "file": "index.eba111bf.css",
+    "resourceType": "style"
+  },
+  "pages/precing.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "precing.aa41ec19.js",
+    "src": "pages/precing.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "precing.41314da8.css": {
+    "file": "precing.41314da8.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-404.da310ae3.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "error-404.18ced855.css": {
+    "file": "error-404.18ced855.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-500.b2676ff4.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "error-500.e60962de.css": {
+    "file": "error-500.e60962de.css",
+    "resourceType": "style"
+  },
+  "pages/index.css": {
+    "resourceType": "style",
+    "file": "index.eba111bf.css",
+    "src": "pages/index.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.e60962de.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "pages/precing.css": {
+    "resourceType": "style",
+    "file": "precing.41314da8.css",
+    "src": "pages/precing.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.18ced855.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/chunks/styles.mjs
@@ -0,0 +1,10 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "pages/precing.vue": () => import('./precing-styles.0344d740.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./error-404-styles.3db9fdd2.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/welcome.vue": () => import('./welcome-styles.84d3e35e.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./error-500-styles.ad145178.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
